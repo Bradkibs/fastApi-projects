89a4476967ad0aa50dd500ff039024779b30e02b
Bradkibs <Bradleykibs@gmail.com>
2023-10-18 23:15:52 +0300

    fixing changes on api

--- FastAPI-Production-Boilerplate/app/schemas/requests/users.py
+++ FastAPI-Production-Boilerplate/app/schemas/requests/users.py
@@ -63,11 +63,6 @@ class LoginUserRequest(BaseModel):
     password: constr(min_length=8, max_length=64, strict=True)
 
 
-    @validator("username")
-    def username_must_not_contain_special_characters(cls, v):
-        if re.search(r"[^a-zA-Z0-9]", v):
-            raise ValueError("Username must not contain special characters")
-        return v
 
     @validator("phone_number")
     def phone_number_must_contain_plus_then_numbers(cls, v):

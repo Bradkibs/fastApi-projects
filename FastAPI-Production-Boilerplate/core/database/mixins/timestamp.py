76c6849dd76f11955a1ce3ed1c3894c1a3355dde
Bradleykibs <Bradleykibwana@gmail.com>
2023-10-22 12:18:24 +0300

    adding timezone to my created at and updated at attributes

--- FastAPI-Production-Boilerplate/core/database/mixins/timestamp.py
+++ FastAPI-Production-Boilerplate/core/database/mixins/timestamp.py
@@ -1,19 +1,18 @@
-# pylint: skip-file
-
-from sqlalchemy import Column, DateTime, func
+from datetime import datetime
+from sqlalchemy import Column, DateTime
 from sqlalchemy.ext.declarative import declared_attr
-
+import pytz
 
 class TimestampMixin:
     @declared_attr
     def created_at(cls):
-        return Column(DateTime, default=func.now(), nullable=False)
+        return Column(DateTime(timezone=True), default=datetime.now(pytz.utc), nullable=False)
 
     @declared_attr
     def updated_at(cls):
         return Column(
-            DateTime,
-            default=func.now(),
-            onupdate=func.now(),
+            DateTime(timezone=True),
+            default=datetime.now(pytz.utc),
+            onupdate=datetime.now(pytz.utc),
             nullable=False,
         )
